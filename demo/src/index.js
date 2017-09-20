b8e9a743dc66769a3453670f8f39cb83ac64f379
Samrith Shankar <samrith@ithaka.travel>
2017-09-21 01:09:50 +0530

    Updated demo

--- demo/src/index.js
+++ demo/src/index.js
@@ -7,6 +7,15 @@ import InputSelect from '../../src';
 class Demo extends Component {
   constructor() {
     super();
+    this.state = {
+      openUp: false,
+      disableEnter: true,
+      collapseOnBlur: true, //Boolean
+      collapseOnEscape: true, //Boolean 
+      collapseOnSelect: true, //Boolean
+      selected: null
+    }
+
     this.options = []
 
     for(let i=0, len=100; i<len; i++)
@@ -16,20 +25,52 @@ class Demo extends Component {
       });
   }
 
-  handleSelect = (option) => {
-    console.log('selected', option);
+  manipState = (state, key, value) => {
+    return Object.assign({}, this.state, {
+      [key]: value
+    });
+  }
+
+  handleChange = (key) => {
+    let state = this.manipState(this.state, key, !this.state[key]);
+    this.setState(state);
   }
 
   render() {
     return <div className="demo-inner">
-      <h1>react-input-select Demo</h1>
+      <h1>react-select-input Demo</h1>
       <InputSelect
         onSelect={this.handleSelect}
         options={this.options}
-        autoFocus
-        collapseOnBlur={true}
-        noOptions={<div className="no-options">No options!</div>}
+        autoFocus={false}
+        openUp={this.state.openUp}
+        disableEnter={this.state.disableEnter}
+        collapseOnBlur={this.state.collapseOnBlur}
+        collapseOnEscape={this.state.collapseOnEscape}
+        collapseOnSelect={this.state.collapseOnSelect}
       />
+      <div className="options">
+        <label>
+          <input type="checkbox" checked={this.state.openUp} onChange={this.handleChange.bind(this, 'openUp')} />
+          Open Up
+        </label>
+        <label>
+          <input type="checkbox" checked={this.state.disableEnter} onChange={this.handleChange.bind(this, 'disableEnter')} />
+          Disable enter
+        </label>
+        <label>
+          <input type="checkbox" checked={this.state.collapseOnBlur} onChange={this.handleChange.bind(this, 'collapseOnBlur')} />
+          Collapse on blur
+        </label>
+        <label>
+          <input type="checkbox" checked={this.state.collapseOnEscape} onChange={this.handleChange.bind(this, 'collapseOnEscape')} />
+          Collapse on escape
+        </label>
+        <label>
+          <input type="checkbox" checked={this.state.collapseOnSelect} onChange={this.handleChange.bind(this, 'collapseOnSelect')} />
+          Collapse on select
+        </label>
+      </div>
     </div>
   }
 }
